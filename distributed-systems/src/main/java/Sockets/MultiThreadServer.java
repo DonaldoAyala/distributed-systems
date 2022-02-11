7948f3680cac1bd5b757c10c2e8517f48595717d
unknown <ayala.segoviano.donaldo@gmail.com>
2022-02-10 21:03:30 -0600

    Implemented multi thread server class

--- distributed-systems/src/main/java/Sockets/MultiThreadServer.java
+++ distributed-systems/src/main/java/Sockets/MultiThreadServer.java
@@ -5,57 +5,77 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Server {
+public class MultiThreadServer {
     private ServerSocket serverSocket;
-    private Socket clientConnection;
-    private DataInputStream inputStream;
-    private DataOutputStream outputStream;
+    private boolean isRunning;
     
     public void start(short port) throws IOException {
         serverSocket = new ServerSocket(port);
+        isRunning = true;
+        Socket newClientConnection;
+        Thread newClientThread;
+        
         System.out.println("Server waiting for connections on port " + port);
-        clientConnection = serverSocket.accept();
-        System.out.println("New connection established");
-        inputStream = new DataInputStream(clientConnection.getInputStream());
-        outputStream = new DataOutputStream(clientConnection.getOutputStream());
-    }
-    
-    public void stop() throws IOException{
+        while(isRunning) {
+            newClientConnection = serverSocket.accept();
+            newClientThread = new ServerThread(newClientConnection);
+            newClientThread.start();
+            System.out.println("New connection established");
+        }
+        
         serverSocket.close();
     }
     
-    public double[] testReceiveDoublesOneByOne() throws IOException {
-        int numberOfDoubles = inputStream.readInt();
-        double[] doubles = new double[numberOfDoubles];
-        
-        for (int i = 0; i < numberOfDoubles; i++) {
-            doubles[i] = inputStream.readDouble();
-        }
-        System.out.println("All floats received");
-        return doubles;
+    public void stop() throws IOException{
+        isRunning = false;
     }
     
-    public double[] testReceiveDoublesInBuffer() throws IOException {
-        int numberOfDoubles = inputStream.readInt();
-        int readBytes;
-        double[] doubles = new double[numberOfDoubles];
-        byte[] rawDataBuffer = new byte[numberOfDoubles * Double.SIZE];
+    class ServerThread extends Thread {
+        private Socket clientConnection;
+        private DataInputStream inputStream;
+        private DataOutputStream outputStream;
         
-        int position = 0;
-        int remainingBytes = numberOfDoubles * Double.SIZE;
-        while (remainingBytes > 0) {
-            readBytes = inputStream.read(rawDataBuffer, position, remainingBytes);
-            position += readBytes;
-            remainingBytes -= readBytes;
+        public ServerThread(Socket clientConnection) throws IOException {
+            this.clientConnection = clientConnection;
+            inputStream = new DataInputStream(clientConnection.getInputStream());
+            outputStream = new DataOutputStream(clientConnection.getOutputStream());
         }
         
-        ByteBuffer byteBuffer = ByteBuffer.wrap(rawDataBuffer);
-        for (int i = 0; i < numberOfDoubles; i++) {
-            doubles[i] = byteBuffer.getDouble();
+        @Override
+        public void run() {
+            try {
+                testReceiveDoublesInBuffer();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
         }
         
-        System.out.println("All floats received");
-        return doubles;
+        public double[] testReceiveDoublesInBuffer() throws IOException {
+            int numberOfDoubles = inputStream.readInt();
+            int readBytes;
+            double[] doubles = new double[numberOfDoubles];
+            byte[] rawDataBuffer = new byte[numberOfDoubles * Double.SIZE];
+
+            int position = 0;
+            int remainingBytes = numberOfDoubles * Double.SIZE;
+            while (remainingBytes > 0) {
+                readBytes = inputStream.read(rawDataBuffer, position, remainingBytes);
+                position += readBytes;
+                remainingBytes -= readBytes;
+            }
+
+            ByteBuffer byteBuffer = ByteBuffer.wrap(rawDataBuffer);
+            for (int i = 0; i < numberOfDoubles; i++) {
+                doubles[i] = byteBuffer.getDouble();
+            }
+
+            System.out.println("All floats received");
+            return doubles;
+        }
     }
+    
+    
 }
