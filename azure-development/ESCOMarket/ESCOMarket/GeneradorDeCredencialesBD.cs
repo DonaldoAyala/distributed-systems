0f4848b984077881e04aff3c57eb4735bbfa71c0
dony_as <ayala.segoviano.donaldo@gmail.com>
2022-06-08 01:08:37 -0500

    Se añade generador de cadena de credenciales

---
+++ azure-development/ESCOMarket/ESCOMarket/GeneradorDeCredencialesBD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESCOMarket
+{
+    public class GeneradorDeCredencialesBD
+    {
+        public static string ObtenerCredencialesBaseDeDatos()
+        {
+            //string Server = Environment.GetEnvironmentVariable("Server");
+            //string UserID = Environment.GetEnvironmentVariable("UserID");
+            //string Password = Environment.GetEnvironmentVariable("Password");
+            //string Database = Environment.GetEnvironmentVariable("Database");
+            string Server = "localhost";
+            string UserID = "donaldo";
+            string Password = "donaldohoracio";
+            string Database = "web_shop";
+            string databaseConnectionCredentials =
+                "Server=" + Server +
+                ";UserID=" + UserID +
+                ";Password=" + Password +
+                ";Database=" + Database +
+                ";SslMode=Preferred;";
+
+            return databaseConnectionCredentials;
+        }
+    }
+}
