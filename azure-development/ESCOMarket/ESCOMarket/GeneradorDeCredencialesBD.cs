32e11a2dcc11148188cdf5583e3ed76c3c491567
dony_as <ayala.segoviano.donaldo@gmail.com>
2022-06-09 01:05:02 -0500

    Changed hardcoded for environment variables

--- azure-development/ESCOMarket/ESCOMarket/GeneradorDeCredencialesBD.cs
+++ azure-development/ESCOMarket/ESCOMarket/GeneradorDeCredencialesBD.cs
@@ -8,14 +8,11 @@ namespace ESCOMarket
     {
         public static string ObtenerCredencialesBaseDeDatos()
         {
-            //string Server = Environment.GetEnvironmentVariable("Server");
-            //string UserID = Environment.GetEnvironmentVariable("UserID");
-            //string Password = Environment.GetEnvironmentVariable("Password");
-            //string Database = Environment.GetEnvironmentVariable("Database");
-            string Server = "localhost";
-            string UserID = "donaldo";
-            string Password = "donaldohoracio";
-            string Database = "web_shop";
+            string Server = Environment.GetEnvironmentVariable("Server");
+            string UserID = Environment.GetEnvironmentVariable("UserID");
+            string Password = Environment.GetEnvironmentVariable("Password");
+            string Database = Environment.GetEnvironmentVariable("Database");
+
             string databaseConnectionCredentials =
                 "Server=" + Server +
                 ";UserID=" + UserID +
